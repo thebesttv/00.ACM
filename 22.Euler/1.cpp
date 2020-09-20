d3fb59c7fa997aae165a0ebe0adb08f63b0adfd4
thebesttv <taojuntian@outlook.com>
2020-09-20 21:24:20 +0800

    Euler 1

---
+++ 22.Euler/1.cpp
@@ -0,0 +1,46 @@
+// Tag: 
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<climits>
+#include<cmath>
+#include<ctime>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<list>
+#include<set>
+#include<map>
+#include<utility>
+#include<algorithm>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define FORR(i,a,b) for(int i=(a);i<=(b);++i)
+#define ROR(i,a,b) for(int i=(a);i>=(b);--i)
+#define RORR(i,a,b) for(int i=(a);i>(b);--i)
+#define PQ priority_queue
+#define VR vector
+#define MST(a,b) memset(a,b,sizeof(a))
+#define FGETS(s) fgets(s,sizeof(s),stdin)
+#define ALL(x) x.begin(),x.end()
+#define INS(x) inserter(x,x.begin())
+#define FI first
+#define SE second
+#define pb push_back
+typedef long long LL;
+typedef long long unsigned LLU;
+typedef pair<int,int> pii;
+
+bool vis[1020];
+
+int main(void){
+  for(int i=3;i<1000;i+=3) vis[i] = 1;
+  for(int i=5;i<1000;i+=5) vis[i] = 1;
+  int s = 0;
+  FOR(i,1,1000) if(vis[i]) s += i;
+  printf("%d\n",s);
+
+  return 0;
+}
